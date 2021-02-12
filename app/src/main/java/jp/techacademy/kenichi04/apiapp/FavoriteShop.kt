6204475799d2e876bbc3152a86171126f9b0cd40
kenichi04 <slow_life_1407@yahoo.co.jp>
2021-02-12 21:54:41 +0900

    api一覧、お気に入り

---
+++ app/src/main/java/jp/techacademy/kenichi04/apiapp/FavoriteShop.kt
@@ -0,0 +1,48 @@
+package jp.techacademy.kenichi04.apiapp
+
+import io.realm.Realm
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class FavoriteShop: RealmObject() {
+    @PrimaryKey
+    var id: String = ""
+    var imageUrl: String = ""
+    var name: String = ""
+    var url: String = ""
+
+    companion object {
+        fun findAll(): List<FavoriteShop> =   // お気に入りのShopを全権取得
+            Realm.getDefaultInstance().use { realm ->
+                realm.where(FavoriteShop::class.java)
+                    .findAll().let {
+                        realm.copyFromRealm(it)
+                    }
+            }
+
+        fun findBy(id: String): FavoriteShop? =  // お気に入りShopをidで検索して返す
+            Realm.getDefaultInstance().use { realm ->
+                realm.where(FavoriteShop::class.java)
+                    .equalTo(FavoriteShop::id.name, id)
+                    .findFirst()?.let {
+                        realm.copyFromRealm(it)
+                    }
+            }
+
+        fun insert(favoriteShop: FavoriteShop) = // お気に入り追加
+            Realm.getDefaultInstance().executeTransaction {
+                it.insertOrUpdate(favoriteShop)
+            }
+
+        fun delete(id: String) = // idでお気に入りから削除
+            Realm.getDefaultInstance().use { realm ->
+                realm.where(FavoriteShop::class.java)
+                    .equalTo(FavoriteShop::id.name, id)
+                    .findFirst()?.also { deleteShop ->
+                        realm.executeTransaction {
+                            deleteShop.deleteFromRealm()
+                        }
+                    }
+            }
+    }
+}
